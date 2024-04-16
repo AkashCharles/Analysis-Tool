bd9bf76b11af0cd0a5346913574f00a0fc3c4e8b
AkashCharles zzz <akash.25it@licet.ac.in>
2024-04-16 06:20:21 +0000

    swot update

--- src/components/OppandThreat.jsx
+++ src/components/OppandThreat.jsx
@@ -1,29 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getFirestore, addDoc, collection } from "firebase/firestore"; 
 import { useEarthoOne } from '@eartho/one-client-react';
-import { Link } from 'react-router-dom';
 import Navbar from "./Navbar";
 import { useNavigate } from 'react-router-dom';
 
 const OppandThreat = () => {
-  const [opportunitiesData, setOpportunitiesData] = useState(getStoredData('opportunities') || [{ opportunity: '', weight: '', rating: '' }]);
-  const [opportunityTableData, setOpportunityTableData] = useState([]);
-  const [showOpportunityTable, setShowOpportunityTable] = useState(false);
-  const [threatsData, setThreatsData] = useState(getStoredData('threats') || [{ threat: '', weight: '', rating: '' }]);
-  const [threatTableData, setThreatTableData] = useState([]);
-  const [showThreatTable, setShowThreatTable] = useState(false);
+  const [opportunitiesData, setOpportunitiesData] = useState([{ opportunity: '', weight: '', rating: '' }]);
+  const [threatsData, setThreatsData] = useState([{ threat: '', weight: '', rating: '' }]);
   const [totalScore, setTotalScore] = useState(0); 
-  const [ifeChecked, setIFEChecked] = useState(false);
   const db = getFirestore();
   const { user } = useEarthoOne();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const storedIFEChecked = localStorage.getItem('ifeChecked');
-    if (storedIFEChecked) {
-      setIFEChecked(true);
-    }
-  }, []);
 
   const handleChange = (index, field, value, isOpportunity) => {
     const newData = isOpportunity ? [...opportunitiesData] : [...threatsData];
@@ -60,29 +47,11 @@ const OppandThreat = () => {
       await Promise.all([...opportunityBatch, ...threatBatch]);
       alert("Data saved to Firestore");
 
-      // Calculate total scores
       const totalOpportunityScore = calculateScore(opportunitiesData);
       const totalThreatScore = calculateScore(threatsData);
       const totalSum = totalOpportunityScore + totalThreatScore;
 
-      // Set the total score state
       setTotalScore(totalSum);
-
-      // Set the table data based on the submitted data
-      const updatedOpportunityTableData = opportunitiesData.map((opportunity) => ({
-        ...opportunity,
-        score: parseFloat(opportunity.weight) * parseInt(opportunity.rating)
-      }));
-      setOpportunityTableData(updatedOpportunityTableData);
-
-      const updatedThreatTableData = threatsData.map((threat) => ({
-        ...threat,
-        score: parseFloat(threat.weight) * parseInt(threat.rating)
-      }));
-      setThreatTableData(updatedThreatTableData);
-
-      setShowOpportunityTable(true);
-      setShowThreatTable(true);
     } catch (error) {
       console.error("Error adding document: ", error);
     }
@@ -97,47 +66,10 @@ const OppandThreat = () => {
     return totalScore;
   };
 
-  const [clickedWord, setClickedWord] = useState('');
-
-  const words = ['IFE', 'EFE', 'CSF', 'Word4', 'Word5', 'Word6', 'Word7', 'Word8'];
-
-  const handleButtonClick = (page) => {
-    navigate(page); 
-  };
-
-  useEffect(() => { 
-    localStorage.setItem('opportunities', JSON.stringify(opportunitiesData));
-    localStorage.setItem('threats', JSON.stringify(threatsData));
-  }, [opportunitiesData, threatsData]);
-
-  function getStoredData(key) {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
-  }
-
   return (
     <div className='bg-slate-700 min-h-screen'>
       <div className='py-4'>
         <Navbar />
-      </div>
-      <div className="flex flex-wrap justify-center gap-2 md:gap-4 my-8">
-        {words.map((word, index) => (
-          <button
-            key={index}
-            className={`px-4 py-2 bg-white text-black rounded-md hover:bg-gray-200 focus:outline-none ${
-              clickedWord === word ? 'font-bold' : ''
-            }`}
-            onClick={() => {
-              if (word === 'IFE') {
-                handleButtonClick('/strength-page'); 
-              } if (word === 'CSF') {
-                handleButtonClick('/csf-page'); 
-              }
-            }}
-          >
-            {word}
-          </button>
-        ))}
       </div>
       <div className="flex flex-col items-center justify-center py-10">
         <h1 className="text-3xl font-bold text-white mb-8">Internal Factor Evaluation</h1>
@@ -228,49 +160,6 @@ const OppandThreat = () => {
           </button>
         </form>
 
-        {(showOpportunityTable || showThreatTable) && (
-          <div className="mt-8 w-full mx-auto">
-            <table className="w-full border-collapse border border-gray-400">
-              <thead>
-                <tr className="bg-gray-200 border border-gray-400">
-                  <th className="px-4 py-2">Opportunity/Threat</th>
-                  <th className="px-4 py-2">Weight</th>
-                  <th className="px-4 py-2">Rating</th>
-                  <th className="px-4 py-2">Score</th>
-                </tr>
-              </thead>
-              <tbody>
-                {showOpportunityTable &&
-                  opportunityTableData.map((item, index) => (
-                    <tr key={index} className="border border-gray-400">
-                      <td className="px-4 py-2">{item.opportunity}</td>
-                      <td className="px-4 py-2">{item.weight}</td>
-                      <td className="px-4 py-2">{item.rating}</td>
-                      <td className="px-4 py-2">{item.score}</td>
-                    </tr>
-                  ))}
-                {showThreatTable &&
-                  threatTableData.map((item, index) => (
-                    <tr key={index} className="border border-gray-400">
-                      <td className="px-4 py-2">{item.threat}</td>
-                      <td className="px-4 py-2">{item.weight}</td>
-                      <td className="px-4 py-2">{item.rating}</td>
-                      <td className="px-4 py-2">{item.score}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-            <div className="mt-4 flex justify-center">
-              <Link to="/csf-page" className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                Go to Other Page
-              </Link>
-            </div>
-          </div>
-        )}
-
-        <div className="mt-4 text-white">
-          Total Score: {totalScore}
-        </div>
       </div>
     </div>
   );
--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getFirestore, addDoc, collection } from "firebase/firestore"; 
 import { useEarthoOne } from '@eartho/one-client-react';
-import { Link } from 'react-router-dom';
 import Navbar from "./Navbar";
 import { useNavigate } from 'react-router-dom';
 
-
-
-const Strength = () => {
-  const [strengthsData, setStrengthsData] = useState(getStoredData('strengths') || [{ strength: '', weight: '', rating: '' }]);
-  const [tableData, setTableData] = useState([]);
-  const [showTable, setShowTable] = useState(false);
-  const [weaknessesData, setWeaknessesData] = useState(getStoredData('weaknesses') || [{ weakness: '', weight: '', rating: '' }]);
-  const [weaknessTableData, setWeaknessTableData] = useState([]);
-  const [showWeaknessTable, setShowWeaknessTable] = useState(false);
-  const [totalScore, setTotalScore] = useState(0);
+const Swot = () => {
+  const [strengthsData, setStrengthsData] = useState([{ strength: '', weight: '', rating: '' }]);
+  const [weaknessesData, setWeaknessesData] = useState([{ weakness: '', weight: '', rating: '' }]);
+  const [opportunitiesData, setOpportunitiesData] = useState([{ opportunity: '', weight: '', rating: '' }]);
+  const [threatsData, setThreatsData] = useState([{ threat: '', weight: '', rating: '' }]);
+  const [totalScore, setTotalScore] = useState(0); 
   const db = getFirestore();
   const { user } = useEarthoOne();
   const navigate = useNavigate();
 
-
-  const handleChange = (index, field, value, isStrength) => {
-    const newData = isStrength ? [...strengthsData] : [...weaknessesData];
+  const handleChange = (index, field, value, category) => {
+    const newData = category === 'strengths' ? [...strengthsData] : category === 'weaknesses' ? [...weaknessesData] : category === 'opportunities' ? [...opportunitiesData] : [...threatsData];
     newData[index][field] = value;
-    isStrength ? setStrengthsData(newData) : setWeaknessesData(newData);
+    category === 'strengths' ? setStrengthsData(newData) : category === 'weaknesses' ? setWeaknessesData(newData) : category === 'opportunities' ? setOpportunitiesData(newData) : setThreatsData(newData);
   };
 
-  const handleAddField = (isStrength) => {
-    const newData = isStrength ? [...strengthsData, { strength: '', weight: '', rating: '' }] : [...weaknessesData, { weakness: '', weight: '', rating: '' }];
-    isStrength ? setStrengthsData(newData) : setWeaknessesData(newData);
+  const handleAddField = (category) => {
+    const newData = category === 'strengths' ? [...strengthsData, { strength: '', weight: '', rating: '' }] : category === 'weaknesses' ? [...weaknessesData, { weakness: '', weight: '', rating: '' }] : category === 'opportunities' ? [...opportunitiesData, { opportunity: '', weight: '', rating: '' }] : [...threatsData, { threat: '', weight: '', rating: '' }];
+    category === 'strengths' ? setStrengthsData(newData) : category === 'weaknesses' ? setWeaknessesData(newData) : category === 'opportunities' ? setOpportunitiesData(newData) : setThreatsData(newData);
+  };
+
+  const handleRemoveField = (index, category) => {
+    const newData = category === 'strengths' ? [...strengthsData] : category === 'weaknesses' ? [...weaknessesData] : category === 'opportunities' ? [...opportunitiesData] : [...threatsData];
+    newData.splice(index, 1);
+    category === 'strengths' ? setStrengthsData(newData) : category === 'weaknesses' ? setWeaknessesData(newData) : category === 'opportunities' ? setOpportunitiesData(newData) : setThreatsData(newData);
   };
 
   const handleSubmit = async (event) => {
@@ -52,32 +52,39 @@ const Strength = () => {
         })
       );
 
-      await Promise.all([...strengthBatch, ...weaknessBatch]);
+      const opportunityBatch = opportunitiesData.map(async (opportunity) =>
+        await addDoc(collection(db, "opportunities"), {
+          displayName: user.displayName,
+          opportunity: opportunity.opportunity,
+          weight: opportunity.weight,
+          rating: opportunity.rating
+        })
+      );
+
+      const threatBatch = threatsData.map(async (threat) =>
+        await addDoc(collection(db, "threats"), {
+          displayName: user.displayName,
+          threat: threat.threat,
+          weight: threat.weight,
+          rating: threat.rating
+        })
+      );
+
+      await Promise.all([...strengthBatch, ...weaknessBatch, ...opportunityBatch, ...threatBatch]);
       alert("Data saved to Firestore");
 
+      const totalOpportunityScore = calculateScore(opportunitiesData);
+      const totalThreatScore = calculateScore(threatsData);
       const totalStrengthScore = calculateScore(strengthsData);
       const totalWeaknessScore = calculateScore(weaknessesData);
-      const totalSum = totalStrengthScore + totalWeaknessScore;
+      const totalSum = totalOpportunityScore + totalThreatScore + totalStrengthScore + totalWeaknessScore;
 
       setTotalScore(totalSum);
-
-      const updatedTableData = strengthsData.map((strength) => ({
-        ...strength,
-        score: parseFloat(strength.weight) * parseInt(strength.rating)
-      }));
-      setTableData(updatedTableData);
-
-      const updatedWeaknessTableData = weaknessesData.map((weakness) => ({
-        ...weakness,
-        score: parseFloat(weakness.weight) * parseInt(weakness.rating)
-      }));
-      setWeaknessTableData(updatedWeaknessTableData);
-
-      setShowTable(true);
-      setShowWeaknessTable(true);
     } catch (error) {
       console.error("Error adding document: ", error);
     }
+
+      navigate('/csf-page');
   };
 
   const calculateScore = (data) => {
@@ -89,124 +96,185 @@ const Strength = () => {
     return totalScore;
   };
 
-  const [clickedWord, setClickedWord] = useState('');
-
-  const words = ['IFE', 'EFE', 'Word3', 'Word4', 'Word5', 'Word6', 'Word7', 'Word8'];
-
-  const handleButtonClick = (page) => {
-    navigate(page); 
-  };
-
-  useEffect(() => { 
-    localStorage.setItem('strengths', JSON.stringify(strengthsData));
-    localStorage.setItem('weaknesses', JSON.stringify(weaknessesData));
-  }, [strengthsData, weaknessesData]);
-
-  function getStoredData(key) {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
-  }
-
   return (
     <div className='bg-slate-700 min-h-screen'>
       <div className='py-4'>
         <Navbar />
-        </div>
-      <div className="flex flex-wrap justify-center gap-2 md:gap-4 my-8">
-        {words.map((word, index) => (
-          <button
-            key={index}
-            className={`px-4 py-2 bg-white text-black rounded-md hover:bg-gray-200 focus:outline-none ${
-              clickedWord === word ? 'font-bold' : ''
-            }`}
-            onClick={() => {
-              if (word === 'IFE') {
-                handleButtonClick('/strength-page'); 
-              } if (word === 'EFE') {
-                handleButtonClick('/other-page'); 
-              }
-            }}
-          >
-            {word}
-          </button>
-        ))}
       </div>
       <div className="flex flex-col items-center justify-center py-10">
-        <h1 className="text-3xl font-bold text-white mb-8">Internal Factor Evaluation</h1>
-        <form className="max-w-[40vw] w-full bg-white p-8 border border-gray-300 rounded-lg" onSubmit={handleSubmit}>
-          <div className="flex flex-col items-center">
-            <div className="w-full mb-8">
+        <h1 className="text-3xl font-bold text-white mb-8">SWOT Analysis</h1>
+        <form className="max-w-[80vw] w-full bg-white p-8 border border-gray-300 rounded-lg" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-2 gap-8">
+            <div>
               <h2 className="text-xl font-bold mb-4">Strengths</h2>
               {strengthsData.map((strength, index) => (
                 <div key={index} className="flex items-center mb-2">
                   <input
                     type="text"
                     value={strength.strength}
-                    onChange={(e) => handleChange(index, 'strength', e.target.value, true)}
+                    onChange={(e) => handleChange(index, 'strength', e.target.value, 'strengths')}
                     placeholder={`Enter strength ${index + 1}`}
                     className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
                   />
                   <input
                     type="number"
                     value={strength.weight}
-                    onChange={(e) => handleChange(index, 'weight', e.target.value, true)}
+                    onChange={(e) => handleChange(index, 'weight', e.target.value, 'strengths')}
                     placeholder={`Weight ${index + 1}`}
                     className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
                   />
                   <input
                     type="number"
                     value={strength.rating}
-                    onChange={(e) => handleChange(index, 'rating', e.target.value, true)}
+                    onChange={(e) => handleChange(index, 'rating', e.target.value, 'strengths')}
                     placeholder={`Rating ${index + 1}`}
                     className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
                   />
-                  {index === strengthsData.length - 1 && (
-                    <button
-                      type="button"
-                      onClick={() => handleAddField(true)}
-                      className="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
-                    >
-                      +
-                    </button>
-                  )}
+                  <button
+                    type="button"
+                    onClick={() => handleAddField('strengths')}
+                    className="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveField(index, 'strengths')}
+                    className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 ml-2"
+                  >
+                    -
+                  </button>
                 </div>
               ))}
             </div>
 
-            <div className="w-full">
+            <div>
               <h2 className="text-xl font-bold mb-4">Weaknesses</h2>
               {weaknessesData.map((weakness, index) => (
-                <div key={index} className="flex gap-2 items-center">
+                <div key={index} className="flex items-center mb-2">
                   <input
                     type="text"
                     value={weakness.weakness}
-                    onChange={(e) => handleChange(index, 'weakness', e.target.value, false)}
+                    onChange={(e) => handleChange(index, 'weakness', e.target.value, 'weaknesses')}
                     placeholder={`Enter weakness ${index + 1}`}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
                   />
                   <input
                     type="number"
                     value={weakness.weight}
-                    onChange={(e) => handleChange(index, 'weight', e.target.value, false)}
+                    onChange={(e) => handleChange(index, 'weight', e.target.value, 'weaknesses')}
                     placeholder={`Weight ${index + 1}`}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
                   />
                   <input
                     type="number"
                     value={weakness.rating}
-                    onChange={(e) => handleChange(index, 'rating', e.target.value, false)}
+                    onChange={(e) => handleChange(index, 'rating', e.target.value, 'weaknesses')}
                     placeholder={`Rating ${index + 1}`}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
                   />
-                  {index === weaknessesData.length - 1 && (
-                    <button
-                      type="button"
-                      onClick={() => handleAddField(false)}
-                      className="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
-                    >
-                      +
-                    </button>
-                  )}
+                  <button
+                    type="button"
+                    onClick={() => handleAddField('weaknesses')}
+                    className="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveField(index, 'weaknesses')}
+                    className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 ml-2"
+                  >
+                    -
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-8 mt-8">
+            <div>
+              <h2 className="text-xl font-bold mb-4">Opportunities</h2>
+              {opportunitiesData.map((opportunity, index) => (
+                <div key={index} className="flex items-center mb-2">
+                  <input
+                    type="text"
+                    value={opportunity.opportunity}
+                    onChange={(e) => handleChange(index, 'opportunity', e.target.value, 'opportunities')}
+                    placeholder={`Enter opportunity ${index + 1}`}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
+                  />
+                  <input
+                    type="number"
+                    value={opportunity.weight}
+                    onChange={(e) => handleChange(index, 'weight', e.target.value, 'opportunities')}
+                    placeholder={`Weight ${index + 1}`}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
+                  />
+                  <input
+                    type="number"
+                    value={opportunity.rating}
+                    onChange={(e) => handleChange(index, 'rating', e.target.value, 'opportunities')}
+                    placeholder={`Rating ${index + 1}`}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleAddField('opportunities')}
+                    className="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveField(index, 'opportunities')}
+                    className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 ml-2"
+                  >
+                    -
+                  </button>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold mb-4">Threats</h2>
+              {threatsData.map((threat, index) => (
+                <div key={index} className="flex items-center mb-2">
+                  <input
+                    type="text"
+                    value={threat.threat}
+                    onChange={(e) => handleChange(index, 'threat', e.target.value, 'threats')}
+                    placeholder={`Enter threat ${index + 1}`}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
+                  />
+                  <input
+                    type="number"
+                    value={threat.weight}
+                    onChange={(e) => handleChange(index, 'weight', e.target.value, 'threats')}
+                    placeholder={`Weight ${index + 1}`}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
+                  />
+                  <input
+                    type="number"
+                    value={threat.rating}
+                    onChange={(e) => handleChange(index, 'rating', e.target.value, 'threats')}
+                    placeholder={`Rating ${index + 1}`}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md mr-2"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleAddField('threats')}
+                    className="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveField(index, 'threats')}
+                    className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 ml-2"
+                  >
+                    -
+                  </button>
                 </div>
               ))}
             </div>
@@ -220,52 +288,9 @@ const Strength = () => {
           </button>
         </form>
 
-        {(showTable || showWeaknessTable) && (
-          <div className="mt-8 w-full mx-auto">
-            <table className="w-full border-collapse border border-gray-400">
-              <thead>
-                <tr className="bg-gray-200 border border-gray-400">
-                  <th className="px-4 py-2">Strength/Weakness</th>
-                  <th className="px-4 py-2">Weight</th>
-                  <th className="px-4 py-2">Rating</th>
-                  <th className="px-4 py-2">Score</th>
-                </tr>
-              </thead>
-              <tbody>
-                {showTable &&
-                  tableData.map((item, index) => (
-                    <tr key={index} className="border border-gray-400">
-                      <td className="px-4 py-2">{item.strength}</td>
-                      <td className="px-4 py-2">{item.weight}</td>
-                      <td className="px-4 py-2">{item.rating}</td>
-                      <td className="px-4 py-2">{item.score}</td>
-                    </tr>
-                  ))}
-                {showWeaknessTable &&
-                  weaknessTableData.map((item, index) => (
-                    <tr key={index} className="border border-gray-400">
-                      <td className="px-4 py-2">{item.weakness}</td>
-                      <td className="px-4 py-2">{item.weight}</td>
-                      <td className="px-4 py-2">{item.rating}</td>
-                      <td className="px-4 py-2">{item.score}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-            <div className="mt-4 flex justify-center">
-              <Link to="/other-page" className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                Go to Other Page
-              </Link>
-            </div>
-          </div>
-        )}
-
-        <div className="mt-4 text-white">
-          Total Score: {totalScore}
-        </div>
       </div>
     </div>
   );
 };
 
-export default Strength;
+export default Swot;
